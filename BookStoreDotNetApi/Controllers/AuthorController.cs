fc155ba5c772cea6cd88530706c1fb2e7d124f85
Sarthak Dhakal <sarthakdhakal03@gmail.com>
2021-09-12 12:31:09 +0545

    first commit

---
+++ BookStoreDotNetApi/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreDotNetApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+// using 
+namespace BookStoreDotNetApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            using (var context = new BookStoresDBContext())
+            {
+                // return context.Authors.ToList();
+                Author author =context.Authors.Where(x => x.FirstName == "Ram").First();
+                // author.FirstName = "Ram";
+                // author.LastName = "Shyam";
+                context.Authors.Remove(author);
+                context.SaveChanges();
+                return context.Authors.Where(x => x.FirstName == "Ram").ToList();
+            }
+        }
+    }
+}
---
+++ BookStoreDotNetApi/Models/Job.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BookStoreDotNetApi.Models
+{
+    public partial class Job
+    {
+        public Job()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public short JobId { get; set; }
+        public string JobDesc { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
